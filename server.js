98776c92b1600dac1b48da5b17967a29ecbb1a48
Your Name <suttonk@bcglobal.net>
2021-01-16 11:47:28 -0800

    passport and sequelize support

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
-const express = require("express");
-const PORT = process.env.PORT || 3306;
-const app = express();
+// Requiring necessary npm packages
+var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+var expHandlebars = require("express-handlebars")
 
-// API routes
-const routes = require("./routes");
-// Models for sequelize
-const db =require("./models");
+// Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
 
-// Middleware for express
-app.use(express.urlencoded({extended: true }));
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Static Directory
 app.use(express.static("public"));
+app.engine("handlebars", expHandlebars({defaultLayout:"main"}))
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Routes
-app.use(routes);
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-// Syncing our sequelize models and then starting our Express app
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, () => {
-      console.log(`🌎 ==> API server now on port ${PORT} !`);
-    });
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
+});
