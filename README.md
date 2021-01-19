6cd6d34c9fed69580c0359f4351e726f4225ae67
Rinzler8806 <shane.rogers916@gmail.com>
2021-01-18 19:22:08 -0800

    merge attempt

---
+++ README.md
@@ -0,0 +1,53 @@
+# motivate-me
+
+Members: Ahmed, Shane, Natan, Keith, Vijay
+
+## Motivational App
+
+Purpose: A simple app to get people through "the best of times and the worst of times." 
+
+Quote generator based on category. Also includes a quote of the day. This app is intended to be used as quick inspiration. 
+
+## Breakdown
+
+- Categories for users
+- Body
+Mind
+Spirit
+Career
+Finance
+Relationships & love
+Family
+Growth
+- Quote of the day
+- Sub categories based on struggles?
+- Your quotes of the day tailored to you
+- Like a weather report, very quick app experience
+
+## Dependencies
+
+- Node
+- Express
+- MySQL
+- Sequelize ORM
+
+## Tools
+
+- Front end: HTML, CSS, Javascript, Bootstrap, jQuery, JSON
+- Back end: Express, Sequelize, MySQL, Handlebars
+- JS library of choice - Lottie or other animation library
+- Api
+*Quotes API
+*Random Quotes API
+*Quote of the Day API
+*Famous Quotes API 
+*MovieQuotes API
+*Bestquotes API
+*PaperQuotes API
+*They Said So API
+*Kutipan API
+*Universal Inspirational Quotes API*
+
+Install all packages from bash command line:
+ npm -y init ; npm i bcryptjs ; npm i dotenv ; npm i express ; npm i express-handlebars ; npm i express-session ; npm i if-env ; npm i mysql2 ; npm i passport ; npm i passport-local ; npm i sequelize ; npm i sequelize-cli
+
--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 "use strict"
 
 var fs = require("fs");
@@ -28,9 +29,46 @@ Object.keys(db).forEach(function(modelName) {
 if (db[modelName].associate) {
   db[modelName].associate(db);
 }
+=======
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+>>>>>>> 3777a7774840fa81dfc726c50c213723c183693b
 });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+<<<<<<< HEAD
 module.exports = db;
+=======
+module.exports = db;
+>>>>>>> 3777a7774840fa81dfc726c50c213723c183693b
